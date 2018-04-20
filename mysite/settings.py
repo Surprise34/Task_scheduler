b563bda4a3dee34f1ec15336d570db2ec848e5ec
Surprise34 <mzkmobile@yandex.ru>
2018-04-20 05:18:19 +0300

    Fixed css bug and registration bug

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,9 +126,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = '/home/alexander/task_scheduler/mysite/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    '/home/alexander/task_scheduler/mysite/static/',    
 ]
+STATIC_URL = '/static/'
+
+
--- mysite/views.py
+++ mysite/views.py
@@ -3,10 +3,6 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
-@login_required
-def home (request):
-    return render(request, 'index.html')
-
 
 def signup(request):
     if request.method == 'POST':
@@ -14,10 +10,11 @@ def signup(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password')
-            user = authenticate(username=username,password=raw_password)
-            login(request, user)
-            return redirect('home')
-        else:
-            form = UserCreationForm()
-        return render(request, 'signup.html', {'form':form})
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:                
+                login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm(request.GET)
+    return render(request, 'signup.html', {'form': form})
